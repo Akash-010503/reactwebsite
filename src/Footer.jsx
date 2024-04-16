33e2603277c69477efa88ccf2b33432c3fdd1680
Akash-010503 <200120107078@git.org.in>
2024-04-16 10:36:18 +0530

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+    const year = new Date().getFullYear();
+
+    return(
+        <>
+            <footer className="w-100 bg-light text-center">
+                <p>© {year} Compatible Solutions. All Rights Reserved | Terms and Condition</p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
---
+++ src/Sdata.jsx
@@ -0,0 +1,36 @@
+import web from "../src/images/c1.jpg";
+import app from "../src/images/c2.jpg";
+import android from "../src/images/c3.jpg";
+import digital from "../src/images/c4.jpg";
+import marketing from "../src/images/c5.jpg";
+import software from "../src/images/c6.jpg";
+
+const Sdata = [
+    {
+        imgsrc: web,
+        title: "web development",
+    },
+    {
+        imgsrc: app,
+        title: "App development",
+    },
+    {
+        imgsrc: software,
+        title: "Software Development",
+    },
+    {
+        imgsrc: digital,
+        title: "Digital Marketing",
+    },
+    {
+        imgsrc: android,
+        title: "Android development",
+    },
+    {
+        imgsrc: marketing,
+        title: "Marketing",
+    },
+];
+
+export default Sdata;
+
